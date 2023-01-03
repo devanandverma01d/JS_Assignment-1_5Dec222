473fdc20b03f674d5a322d43124c92a7f196271f
Devanand Verma <devanandverma01d@gmail.come>
2023-01-03 12:32:39 +0530

    new

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+var news3 = {   // this is an object
+    //1. property:value
+    w1: "The",
+    w2: 'quick',
+    w3: "brown",
+    w4: `fox`,
+    w5: "jump",
+    w6: 'over',
+    w7: "the",
+    w8: 'lazy',
+    w9: "dog"
+    //2.
+
+    //2.
+}
+
+// object.property    thgis is a variable to store all information
+
+var publish3 = news3.w1 + " " + news3.w2 + ' ' + news3.w3 + ` ` + news3.w4 + " " + news3.w5 + ' ' + news3.w6 + " " + news3.w7 + " " + news3.w8 + ' ' + news3.w9;
+console.log(publish3);
+// lets see the the other method
+
+alert(publish3);
+
+//this is 3rd method to log/display js code
